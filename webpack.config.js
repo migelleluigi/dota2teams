bec4bc8253ed61eeae0de0382619ae720e62b892
migelleluigi <migelle.luigi.ramos@gmail.com>
2016-10-21 01:09:45 +0800

    use webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: "./app/App.js",
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+    ]
+  },
+  devServer: {
+    contentBase: "./public",
+    hot: true
+  },
+}
